a6b84a2e296ca934dabfa2440e3db4d573b08f4f
ysj <2119656347@qq.coom>
2023-04-17 13:11:11 +0800

    Site updated: 2023-04-17 13:10:59

--- js/fomal.js
+++ js/fomal.js
@@ -3170,19 +3170,19 @@ let unsplash = "url(https://source.unsplash.com/random/1920x1080/)";
 
 
 // 主页背景(自己的代码)
-if (localStorage.getItem("blogbg") != undefined) {
-  setBg(localStorage.getItem("blogbg"));
+if (true) {
+  setBg(localStorage.getItem("black"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
     // --default-bg: url(/image/default_cover_14.webp);
-    // --default-bg: url(/image/back2.jpg);
-    // --darkmode-bg:url(/image/yuanshen1.webp);
-    // --mobileday-bg: url(/image/snow.webp);
-    // --mobilenight-bg: url(/image/mb8.webp);
-    --default-bg: black;
-    --darkmode-bg:black;
-    --mobileday-bg: black;
-    --mobilenight-bg: black;
+    --default-bg: url(/image/back2.jpg);
+    --darkmode-bg:url(/image/yuanshen1.webp);
+    --mobileday-bg: url(/image/snow.webp);
+    --mobilenight-bg: url(/image/mb8.webp);
+    // --default-bg: black;
+    // --darkmode-bg:black;
+    // --mobileday-bg: black;
+    // --mobilenight-bg: black;
   }`;
 }
 // 切换背景主函数
